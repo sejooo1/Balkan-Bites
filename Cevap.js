a6b5afeb2589e65c246216c88ced8692ea685bd0
Alden <aldenefendic0@gmail.com>
2023-04-12 22:55:55 +0200

    header for new website

--- Cevap.js
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("cevap");
-const body = document.getElementById("burek");
-button.addEventListener("click", function(){
-    body.style.backgroundImage=URL("Cevapi1-1.jpg");
-})
