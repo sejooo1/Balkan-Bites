0c9c3d1cb8a0cf7a2a1fa9194deeae9f55e183fb
Alden <aldenefendic0@gmail.com>
2023-04-12 18:10:27 +0200

    test

---
+++ Cevap.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("cevap");
+const body = document.getElementById("burek");
+button.addEventListener("click", function(){
+    body.style.backgroundImage=URL("Cevapi1-1.jpg");
+})
